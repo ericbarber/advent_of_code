98a36073c2f210872199a7afe1893baef85659db
ericbarber <eric.a.barber@gmail.com>
2023-12-09 20:56:22 -0700

    day 4 2 star

--- year_2023/day_04/src/bin/part_2.rs
+++ year_2023/day_04/src/bin/part_2.rs
@@ -1,142 +1,98 @@
-use std::fs;
-use std::collections::HashSet;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
-fn main() {
-    println!("Day 3 part 2");
-    let input = fs::read_to_string("./data/input.txt").expect("Failed to read input file");
-    let result = sum_gear_ratios(&input);
-    println!("Sum of all gear ratios: {}", result);
+/// Main function that reads scratchcard data from a file and prints the total number of scratchcards.
+fn main() -> io::Result<()> {
+    let path = "./data/input.txt";
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // New code to print the first three lines
+    for (index, line) in reader.lines().enumerate() {
+        let line = line?;
+        if index < 3 {
+            println!("{}", line);
+        } else {
+            break;
+        }
+    }
+
+    // Process the cards
+    let file = File::open(path)?; // Re-open the file as the previous reader is consumed
+    let reader = BufReader::new(file);
+    let total_cards = process_cards(reader.lines());
+    println!("Total number of scratchcards: {}", total_cards);
+    Ok(())
 }
 
-fn sum_gear_ratios(input: &str) -> i32 {
-    let lines: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    let mut sum = 0;
-    let mut counted_positions = HashSet::new();
+/// Processes scratchcard data from an iterator over lines.
+///
+/// # Arguments
+///
+/// * `lines` - An iterator over `Result<String, io::Error>`, typically lines from a file.
+///
+/// # Returns
+///
+/// * The total number of scratchcards, including originals and copies.
+fn process_cards<I>(lines: I) -> usize
+where
+    I: Iterator<Item = io::Result<String>>,
+{
+    // Parse each line to create a list of cards
+    let mut cards = Vec::new();
 
-    for (y, row) in lines.iter().enumerate() {
-        for (x, &ch) in row.iter().enumerate() {
-            if ch == '*' {
-                let mut part_numbers = Vec::new();
+    for line in lines {
+        if let Ok(card) = line {
+            let card_data = card.split(':').nth(1).unwrap_or("").trim();
+            let parts: Vec<_> = card_data.split('|').collect();
+            let left: Vec<_> = parts[0]
+                .trim()
+                .split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect();
+            let right: Vec<_> = parts[1]
+                .trim()
+                .split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect();
+            cards.push((left, right));
+        }
+    }
 
-                // Check all adjacent positions
-                for dy in -1..=1 {
-                    for dx in -1..=1 {
-                        if dy == 0 && dx == 0 { continue; } // Skip the gear itself
-                        let adj_x = x as i32 + dx;
-                        let adj_y = y as i32 + dy;
+    // Process the cards to calculate total number
+    let mut total_cards = 0;
+    let mut to_process = vec![1; cards.len()];
 
-                        if counted_positions.contains(&(adj_x, adj_y)) {
-                            continue; // Skip if this position is already counted
-                        }
+    for i in 0..cards.len() {
+        let (ref left, ref right) = cards[i];
+        let matches = left.iter().filter(|&&n| right.contains(&n)).count();
+        total_cards += to_process[i];
 
-                        if let Some(number) = get_full_number_at(&lines, adj_x, adj_y) {
-                            part_numbers.push(number);
-                            mark_number_as_counted(&lines, adj_x, adj_y, &mut counted_positions);
-                        }
-                    }
-                }
-
-                // If exactly two part numbers are found, multiply them and add to sum
-                if part_numbers.len() == 2 {
-                    sum += part_numbers[0] * part_numbers[1];
-                }
+        for j in 1..=matches {
+            if i + j < cards.len() {
+                to_process[i + j] += to_process[i];
             }
         }
     }
 
-    sum
-}
-
-fn mark_number_as_counted(lines: &[Vec<char>], x: i32, y: i32, counted: &mut HashSet<(i32, i32)>) {
-    // Mark the positions of the digits of the number starting at (x, y) as counted
-    if x < 0 || y < 0 || y as usize >= lines.len() || x as usize >= lines[y as usize].len() {
-        return;
-    }
-    if !lines[y as usize][x as usize].is_digit(10) {
-        return;
-    }
-
-    let mut x_pos = x;
-    while x_pos < lines[y as usize].len() as i32 && lines[y as usize][x_pos as usize].is_digit(10) {
-        counted.insert((x_pos, y));
-        x_pos += 1;
-    }
-}
-
-fn get_full_number_at(lines: &[Vec<char>], x: i32, y: i32) -> Option<i32> {
-    if x < 0 || y < 0 || y as usize >= lines.len() || x as usize >= lines[y as usize].len() {
-        return None;
-    }
-    if !lines[y as usize][x as usize].is_digit(10) {
-        return None;
-    }
-
-    let mut num = String::new();
-    let mut x_pos = x;
-
-    // Move left to the start of the number
-    while x_pos >= 0 && lines[y as usize][x_pos as usize].is_digit(10) {
-        x_pos -= 1;
-    }
-    x_pos += 1; // Move back to the first digit
-
-    // Extract the full number
-    while x_pos < lines[y as usize].len() as i32 && lines[y as usize][x_pos as usize].is_digit(10) {
-        num.push(lines[y as usize][x_pos as usize]);
-        x_pos += 1;
-    }
-
-    Some(num.parse::<i32>().unwrap_or(0))
+    total_cards
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    /// Tests the `process_cards` function with a predefined set of scratchcard data.
     #[test]
-    fn test_sum_gear_ratios() {
-        let test_input = 
-            "467..114..\n\
-             ...*......\n\
-             ..35..633.\n\
-             ......#...\n\
-             617*......\n\
-             .....+.58.\n\
-             ..592.....\n\
-             ......755.\n\
-             ...$.*....\n\
-             .664.598..";
+    fn test_scratchcards() {
+        let input_data = " Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        let lines = input_data.split('\n').map(String::from).map(Ok);
 
-        let expected_result = 467835; // Sum of gear ratios in the example
-        let actual_result = sum_gear_ratios(&test_input);
-
-        assert_eq!(actual_result, expected_result);
+        assert_eq!(process_cards(lines), 30);
     }
 }
-
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn test_sum_part_numbers() {
-//         let test_input = 
-//             "467..114..\n\
-//              ...*......\n\
-//              ..35..633.\n\
-//              ......#...\n\
-//              617*......\n\
-//              .....+.58.\n\
-//              ..592.....\n\
-//              ......755.\n\
-//              ...$.*....\n\
-//              .664.598..";
-//
-//         let expected_result = 4361; // Sum of part numbers in the example
-//         let actual_result = sum_part_numbers(&test_input);
-//
-//         assert_eq!(actual_result, expected_result);
-//     }
-// }
-//
